5dabfe2e5c5b88f78c94e2fc7f6055ef7e27f848
saulotarsobc <saulotarsobc@gmail.com>
2024-04-11 15:59:45 -0300

    :fire:update: profile screen

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Help
 
+- [atualizar informações do user](https://firebase.google.com/docs/auth/web/manage-users?hl=pt-br)
 - [nativebase - doc](https://docs.nativebase.io/)
 - [Implementing Firebase Authentication in React Native App with Expo (a detailed explanation)](https://medium.com/@adityasinghrathore360/implementing-firebase-authentication-in-react-native-app-with-expo-a-detailed-explanation-cea4d1113501)
 - [styled-components - React Native](https://styled-components.com/docs/basics#react-native)
--- src/screens/AccountScreen.tsx
+++ src/screens/AccountScreen.tsx
@@ -1,102 +1,216 @@
 import { useNavigation } from '@react-navigation/native';
-import { signOut } from 'firebase/auth';
-import { useContext } from 'react';
-import { Image, Platform, ScrollView, Text, TouchableOpacity, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { auth } from '../../firebaseConfig';
+import { signOut, updateProfile } from 'firebase/auth';
+import { useContext, useEffect, useState } from 'react';
+import { Image, Linking, Text, TouchableOpacity, View } from 'react-native';
 import { UserContext } from '../contexts/UserContext';
 import { handleDelete } from '../firebase';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { IconLogout2, IconEdit, IconChevronLeft, IconMenuDeep } from 'tabler-icons-react-native';
+import {
+  IconLogout2,
+  IconChevronLeft,
+  IconPhoneCall,
+  IconTrashX,
+  IconChevronRight,
+  IconBug,
+  IconWorld,
+  IconBrandWhatsapp,
+  IconMenuDeep,
+} from 'tabler-icons-react-native';
 import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import * as DocumentPicker from 'expo-document-picker';
 import { StatusBar } from 'expo-status-bar';
 
-const ios = Platform.OS == 'ios';
-const topMargin = ios ? 'mt-3' : 'mt-10';
+import { auth } from '../../firebaseConfig';
 
 export function AccountScreen() {
-  const user = useContext(UserContext);
   const navigation = useNavigation<NativeStackNavigationProp<any>>();
+  const [displayName, setDisplayName] = useState<null | string>(null);
+  const [photoURL, setphotoURL] = useState<null | string>(null);
+  const [userBio, setuserBio] = useState<null | string>(null);
+
+  const user = useContext(UserContext);
+
+  // todo: enviar a imagem para o firestore e salvar a url no user.photoURL
+  async function updateUrlPhoto(newUrl: string) {
+    updateProfile(auth.currentUser, { photoURL: newUrl })
+      .then(() => {
+        setphotoURL(user.photoURL);
+      })
+      .catch((e) => console.error(e));
+  }
+
+  async function updateDisplayName(newName: string) {
+    updateProfile(auth.currentUser, { displayName: newName })
+      .then(() => {
+        setDisplayName(user.displayName);
+      })
+      .catch((e) => console.error(e));
+  }
+
+  // todo: atualizar a bio do usuario no firebase database
+  async function updateUserBio(bioText: string) {
+    setuserBio(bioText);
+  }
+
+  async function handleBtnMenu() {
+    console.log('>> teste handleBtnMenu');
+    updateUrlPhoto('https://picsum.photos/id/3/200/300');
+
+    updateDisplayName('Saulo B. C.');
+
+    updateUserBio(
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia illo maxime culpa fugiat! Architecto eius rem obcaecati! Veniam optio quibusdam quos, perspiciatis provident dolorum mollitia voluptate! Nesciunt ullam officia nihil.'
+    );
+  }
+
+  useEffect(() => {
+    if (user) {
+      setDisplayName(user.displayName);
+      setphotoURL(user.photoURL);
+    } else {
+      navigation.replace('Welcome');
+    }
+  }, [displayName, photoURL]);
 
   const handleEditPhoto = async () => {
     // escolha a foto
-    const files: DocumentPicker.DocumentPickerResult = await DocumentPicker.getDocumentAsync({
+    const files = await DocumentPicker.getDocumentAsync({
       multiple: false,
       type: 'image/*',
       copyToCacheDirectory: true,
     });
     console.log(files);
+
+    // fazer upload para o firestore
+    // recarregar o user na tela
   };
+
+  const optionsMenu = [
+    {
+      title: 'Arte Ensaio Website',
+      icon: <IconWorld height={wp(8)} width={wp(8)} color="white" />,
+      onPress: () => {
+        Linking.openURL('https://arteensaio.com.br/')
+          .then((data) => {
+            console.log({ data });
+          })
+          .catch((e: Error) => {
+            console.log(e.message);
+          });
+      },
+    },
+    {
+      title: 'Relatar Bug',
+      icon: <IconBug height={wp(8)} width={wp(8)} color="white" />,
+      onPress: () => {
+        Linking.openURL(
+          'mailto:arteensaio@arteensaio.com.br?subject="Relar um bug..."&body=Descrição:%20'
+        ).catch((e: Error) => {
+          console.log(e.message);
+        });
+      },
+    },
+    {
+      title: 'Preciso de Suporte',
+      icon: <IconPhoneCall height={wp(8)} width={wp(8)} color="white" />,
+      onPress: () => {
+        Linking.openURL('tel:552122598282').catch((e: Error) => {
+          console.log(e.message);
+        });
+      },
+    },
+    {
+      title: 'Nosso Whatsapp',
+      icon: <IconBrandWhatsapp height={wp(8)} width={wp(8)} color="white" />,
+      onPress: () => {
+        Linking.openURL(
+          'https://wa.me/5521971134917?text=Olá+%2AArte+Ensaio%21%2A+Preciso+de+mais+informações+sobre+Arte+Ensaio%0AApp+%60%60%60%3Csp%2Fclick%3E%60%60%60'
+        ).catch((e: Error) => {
+          console.log(e.message);
+        });
+      },
+    },
+    {
+      title: 'Apagar minha conta',
+      icon: <IconTrashX height={wp(8)} width={wp(8)} color="white" />,
+      onPress: handleDelete,
+    },
+  ];
 
   return (
     <>
       <StatusBar backgroundColor="#000" style="light" />
 
       <View id="main" className="bg-black relative h-screen w-screen">
-        <View id="header" className="bg-[#111] pt-8 px-1 rounded-bl-3xl rounded-br-3xl">
+        <View id="header" className="bg-[#111] pt-8 rounded-bl-3xl rounded-br-3xl">
           <View id="btns" className="flex flex-row justify-between items-center">
-            <TouchableOpacity className="rounded-md" onPress={() => navigation.goBack()}>
+            <TouchableOpacity className="rounded-md w-11" onPress={() => navigation.goBack()}>
               <IconChevronLeft height={wp(10)} width={wp(10)} color="white" />
             </TouchableOpacity>
 
-            <Text className="text-white text-xl ml-2">Perfil</Text>
+            <Text className="text-white text-xl">Perfil</Text>
 
-            <TouchableOpacity className="rounded-md" onPress={() => navigation.goBack()}>
+            <TouchableOpacity className="rounded-md w-11" onPress={handleBtnMenu}>
               <IconMenuDeep height={wp(10)} width={wp(10)} color="white" />
             </TouchableOpacity>
           </View>
 
-          <View id="avatarAndName" className="translate-y-6">
-            <View id="avatar" className="flex flex-row justify-center">
-              <View className="relative">
+          <View id="avatarAndName" className="translate-y-5">
+            <View id="avatarContainer" className="flex flex-row justify-center">
+              <View className="relative rounded-full border border-white">
                 <Image
-                  source={require('../../assets/images/avatar.png')}
+                  id="profilePhoto"
+                  className="rounded-full overflow-hidden"
+                  source={photoURL ? { uri: photoURL } : require('../../assets/images/avatar.png')}
                   style={{ height: wp(33), width: wp(33) }}
-                  className="rounded-full"
                 />
-
-                {/* btn edit */}
-                <View className="absolute rounded-full bottom-1 right-1 bg-white border-4 border-[#111]">
-                  <TouchableOpacity className="p-1" onPress={handleEditPhoto}>
-                    <IconEdit className="" height={wp(5)} width={wp(5)} color="#111" />
-                  </TouchableOpacity>
-                </View>
               </View>
             </View>
 
-            <View className="w-full flex items-center translate-y-3">
-              <Text className="text-2xl text-white">Saulo Costa</Text>
+            <View id="nameContainer" className="w-full flex items-center translate-y-3">
+              {displayName ? (
+                <Text className="text-2xl text-white">{displayName}</Text>
+              ) : (
+                <Text className="text-2xl text-white opacity-50">sem nome</Text>
+              )}
             </View>
           </View>
         </View>
 
-        <View id="body" className="px-5 py-16">
-          <Text className="text-white mt-3">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus autem dolorum ea,
-            debitis architecto tenetur optio sint illum ab veniam maxime itaque id totam dicta hic
-            sapiente minus qui velit.
-          </Text>
-          <Text className="text-white mt-3">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus autem dolorum ea,
-            debitis architecto tenetur optio sint illum ab veniam maxime itaque id totam dicta hic
-            sapiente minus qui velit.
-          </Text>
-          <Text className="text-white mt-3">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus autem dolorum ea,
-            debitis architecto tenetur optio sint illum ab veniam maxime itaque id totam dicta hic
-            sapiente minus qui velit.
-          </Text>
-          <Text className="text-white mt-3">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus autem dolorum ea,
-            debitis architecto tenetur optio sint illum ab veniam maxime itaque id totam dicta hic
-            sapiente minus qui velit.
-          </Text>
+        <View id="body" className="px-3 py-10">
+          {/* MENU */}
+
+          <View id="userBio" className="bg-[#111] rounded-md mb-3 p-2">
+            {userBio ? (
+              <Text className="text-white text-center"> {userBio} </Text>
+            ) : (
+              <Text className="text-white text-center opacity-50">
+                Que tal adicionar um bio no seu perfil?
+              </Text>
+            )}
+          </View>
+
+          <View id="menuList">
+            {optionsMenu?.map((option: any, index: number) => (
+              <TouchableOpacity
+                onPress={option.onPress}
+                key={`menu-option-${index}`}
+                className="bg-[#111] h-14 rounded-md flex flex-row items-center justify-between p-4 mt-1"
+              >
+                <View className="flex flex-row items-center">
+                  {option.icon}
+                  <Text className="text-white ml-3 text-lg">{option.title}</Text>
+                </View>
+
+                <IconChevronRight height={wp(10)} width={wp(10)} color="#5e5e5e" />
+              </TouchableOpacity>
+            ))}
+          </View>
         </View>
 
         <TouchableOpacity
           id="btnLogout"
-          className="bg-[#111] h-16 flex flex-row justify-center items-center absolute bottom-0 w-screen rounded-tl-3xl rounded-tr-3xl"
+          className="bg-[#111] h-14 flex flex-row justify-center items-center absolute bottom-0 w-screen rounded-tl-3xl rounded-tr-3xl"
           onPress={() => {
             signOut(auth);
             navigation.replace('Welcome');
@@ -108,16 +222,4 @@ export function AccountScreen() {
       </View>
     </>
   );
-}
-
-{
-  /* <TouchableOpacity
-              onPress={() => {
-                handleDelete(() => {
-                  navigation.navigate('Welcome');
-                });
-              }}
-            >
-              <Text className="text-white">Delete</Text>
-            </TouchableOpacity> */
 }
